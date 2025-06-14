4a5174e7060e01f52ec00c6e77ed867aab25adaf
Fernandez-Miguel-A <fernandez.m.alejandro@gmail.com>
2025-06-14 15:05:17 -0300

    Si elimino la etiqueta 'const' ya no se congela la simulacion.

--- Teclado__Proteus/Teclado__stm32_Proteus/Core/Src/keypadmodify.c
+++ Teclado__Proteus/Teclado__stm32_Proteus/Core/Src/keypadmodify.c
@@ -10,17 +10,17 @@ const char keys[4][4] = {{'7','8','9','%'},
 char Keypad_Get_Char(void)
 {
     val_key = 0;
-    const GPIO_TypeDef* R_PORTS[4] = {R1_GPIO_Port, R2_GPIO_Port, R3_GPIO_Port, R4_GPIO_Port};
-    const uint16_t R_PINS[4] = {R1_Pin, R2_Pin, R3_Pin, R4_Pin};
-    const GPIO_TypeDef* C_PORTS[4] = {C1_GPIO_Port, C2_GPIO_Port, C3_GPIO_Port, C4_GPIO_Port};
-    const uint16_t C_PINS[4] = {C1_Pin, C2_Pin, C3_Pin, C4_Pin};
+    GPIO_TypeDef* R_PORTS[4] = {R1_GPIO_Port, R2_GPIO_Port, R3_GPIO_Port, R4_GPIO_Port};
+    uint16_t R_PINS[4] = {R1_Pin, R2_Pin, R3_Pin, R4_Pin};
+    GPIO_TypeDef* C_PORTS[4] = {C1_GPIO_Port, C2_GPIO_Port, C3_GPIO_Port, C4_GPIO_Port};
+    uint16_t C_PINS[4] = {C1_Pin, C2_Pin, C3_Pin, C4_Pin};
 
     for (int r = 0; r < 4; r++) {
         HAL_GPIO_WritePin(R_PORTS[r], R_PINS[r], GPIO_PIN_RESET);
         for (int c = 0; c < 4; c++) {
             if (!(HAL_GPIO_ReadPin(C_PORTS[c], C_PINS[c]))) {
                 HAL_Delay(4);
-                //while (!(HAL_GPIO_ReadPin(C_PORTS[c], C_PINS[c])));
+                while (!(HAL_GPIO_ReadPin(C_PORTS[c], C_PINS[c])));
                 HAL_Delay(4);
                 val_key = keys[r][c];
             }
