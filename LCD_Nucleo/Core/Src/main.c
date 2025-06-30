da783a4ca1f697248ea1745082a3a37a916e63e9
Fernandez-Miguel-A <fernandez.m.alejandro@gmail.com>
2025-06-30 20:58:36 -0300

    Proyecto n1, Nucleo-446re. LCD16X2.

---
+++ LCD_Nucleo/Core/Src/main.c
@@ -0,0 +1,420 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "lcd.h"
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim4;
+
+/* USER CODE BEGIN PV */
+const char figura_1[8] = {0x0A, 0x0A, 0x0A, 0x00, 0x11, 0x11, 0x0E, 0x00};
+const char figura_2[8] = {0x0, 0x4, 0x2, 0x1f, 0x2, 0x4, 0x0, 0x00};
+
+char txt3[] = "LCD key: ";
+char Txt[20], buffer[50], c='M';
+
+float t;
+unsigned int v;
+#define  __delay_ms(x)    HAL_Delay(x)
+
+
+void __delay_us(int32_t delay)
+{
+		uint16_t tickstart;
+		//__HAL_TIM_SetCounter(&htim4, 0);
+		tickstart = __HAL_TIM_GET_COUNTER(&htim4);
+    while((__HAL_TIM_GET_COUNTER(&htim4)-tickstart) < delay);
+}
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM4_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+	int t1=1000, t2=50, _t=0;
+	int x;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+
+	  HAL_TIM_Base_Start(&htim4);
+		__HAL_TIM_SetCounter(&htim4, 0);
+	
+	
+		Lcd_Init();                        // Initialize Lcd
+		HAL_Delay(15);
+		
+		Lcd_CGRAM_Init();
+		Lcd_CGRAM_CreateChar(0, figura_1);
+		Lcd_CGRAM_CreateChar(1, figura_2);
+		Lcd_CGRAM_Close();
+
+
+		Lcd_Clear(); //Lcd_Cmd(_LCD_CLEAR);               // Clear display
+		Lcd_NoBlink(); //Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
+
+
+		Lcd_Set_Cursor(1,1);
+		Lcd_Write_String(txt3);                 // Write text in first row 
+		Lcd_Set_Cursor(2,1);
+		Lcd_Write_String("PIC  es la onda!");                 // Write text in first row
+	
+		HAL_Delay(1000);
+		Lcd_Clear();
+		Lcd_Set_Cursor(2, 1);
+		sprintf(Txt, "  %0.2f     %c", 3.14159265, c);
+		Lcd_Write_String(Txt); 
+
+		Lcd_Set_Cursor(1, 8);
+		Lcd_Write_Char('z');
+		
+		HAL_Delay(1500);
+		Lcd_Clear();
+		
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+	  if(HAL_GPIO_ReadPin(BOTON_GPIO_Port, BOTON_Pin) == 0){
+		  _t = t1;
+		  HAL_Delay(4);
+	  }else {
+		  _t = t2;
+	  }
+	  HAL_Delay(_t);
+		
+		x= 0;/**/
+		//old
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, x);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, x);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, x);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, x);
+		
+		x=1;
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, x);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, x);
+		/*
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, x);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, x);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, x);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, x);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, x);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, x);*/
+		
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		    Lcd_Set_Cursor(1,1);
+        Lcd_Write_String("Test LCD 16x2");
+        Lcd_Set_Cursor(2,1);
+        Lcd_Write_String("PIC18F4550");
+        Lcd_Set_Cursor(2,12);
+        Lcd_Blink();
+        __delay_ms(2000);
+        Lcd_NoBlink();
+        Lcd_Clear();
+        __delay_ms(400);
+        
+        Lcd_Set_Cursor(1,1);
+        Lcd_Write_String("CGRAM Caracteres");
+        Lcd_Set_Cursor(2,1);
+        Lcd_Write_Char(0);
+        Lcd_Write_Char(1);
+        __delay_ms(1500);
+        Lcd_Clear();
+        __delay_ms(400);
+        
+        Lcd_Set_Cursor(1,1);
+        Lcd_Write_String("Hola a todos");
+        __delay_ms(500);
+        for(char i=0; i<15; i++){
+            Lcd_Shift_Right();
+            __delay_ms(300);
+        }
+        __delay_ms(300);
+        for(char i=0; i<15; i++){
+            Lcd_Shift_Left();
+            __delay_ms(300);
+        }
+        Lcd_Clear();
+        __delay_ms(400);
+        
+        Lcd_Set_Cursor(1,1);
+        sprintf(buffer, "Int: %u", v);
+        Lcd_Write_String(buffer);
+        Lcd_Set_Cursor(2,1);
+        sprintf(buffer, "Float: %0.2f", t);
+        Lcd_Write_String(buffer);
+        __delay_ms(1500);
+        Lcd_Clear();
+        __delay_ms(100);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 16;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 65535;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|LED_Pin|GPIO_PIN_8|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_10|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : BOTON_Pin */
+  GPIO_InitStruct.Pin = BOTON_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(BOTON_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC0 PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA0 PA1 PA2 PA3
+                           PA4 LED_Pin PA8 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|LED_Pin|GPIO_PIN_8|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB10 PB3 PB4
+                           PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_10|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
